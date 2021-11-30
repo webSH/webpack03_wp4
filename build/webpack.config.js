dbb7df14297d17323cfa7386ec8e3c562cf944e9
liliang <20386028@qq.com>
2021-11-30 11:11:28 +0800

    多入口文件配置

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -3,14 +3,28 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	mode: 'development', // 开发模式
-	entry: path.resolve(__dirname, '../src/main.js'), // 单入口文件
+	// entry: path.resolve(__dirname, '../src/main.js'), // 单入口文件
+	entry: {
+		main: path.resolve(__dirname, '../src/main.js'), // 入口命名，供 plugins 中使用
+		head: path.resolve(__dirname, '../src/head.js') // 入口命名，供 plugins 中使用
+	},
 	output: {
 		filename: '[name].[hash:8].js', // 打包后文件名称
 		path: path.resolve(__dirname, '../dist') // 打包后的目录
 	},
 	plugins: [
+		// new HtmlWebpackPlugin({
+		// 	template: path.resolve(__dirname, '../public/index.html')
+		// })  //指定插件处理文件的路径(针对单入口文件)
+		new HtmlWebpackPlugin({ //(多入口文件，每个文件一个实例)
+			template: path.resolve(__dirname, '../public/index.html'), //指定插件处理文件的路径
+			filename: 'index.html', // 命名
+			chunks: ['main'] // 与入口文件对应的模块名
+		}),
 		new HtmlWebpackPlugin({
-			template: path.resolve(__dirname, '../public/index.html') //指定插件处理的文件路径
+			template: path.resolve(__dirname, '../public/head.html'),
+			filename: 'head.html',
+			chunks: ['head'] // 可引入多个 chunks（['head','main']），但顺序不一定和数组一致
 		})
 	]
 }
--- readMe.md
+++ readMe.md
@@ -94,4 +94,35 @@ module.exports = {
 `npm run build` 生成如下目录：
 >- dist
 >	- main.95bf8b8f.js
->	- index.html  (main.95bf8b8f.js 会自动插入到 <body>标签最后)
+>	- index.html  (main.95bf8b8f.js 会自动插入到 <body>标签最后
+
+### 3.4 多入口文件的配置
+> 生成多个 html-webpack-plugin 实例来解决这个问题
+webpack.config.js
+```js
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = {
+	mode: 'development', //开发模式
+	entry: {
+		main: path.resolve(__dirname, '../src/main.js'), // 入口命名，供 plugins 中使用
+		head: path.resolve(__dirname, '../src/head.js') // 入口命名，供 plugins 中使用
+	},
+	output: {
+		filename: '[name].[hash:8].js', //打包后的文件名称
+		path: path.resolve(__dirname, '../dist') // 打包后的目录
+	},
+	plugins: [
+		new HtmlpackPlugin({
+			template: path.resolve(__dirname, '../public/index.html'), //指定插件处理的文件路径
+			filename: 'index.html',
+			chunks: ['main'] // 与入口文件对应的模块名
+		}),
+		new HtmlpackPlugin({
+			template: path.resove(__dirname, '../public/head.html'),
+			filename: 'head.html',
+			chunks: ['head'] // 与入口文件对应的模块名
+		})
+	]
+}
+```)
